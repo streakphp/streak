4bc70f1c002858167cf8f9ffa6daf5c889ead299
Alan Gabriel Bem <alan.bem@gmail.com>
2017-11-24 00:53:48 +0100

    improved testing tooling

--- src/Infrastructure/Testing/CommandTestCase.php
+++ src/Infrastructure/Testing/CommandTestCase.php
@@ -23,63 +23,25 @@ use Streak\Infrastructure\Repository\UnitOfWork;
  */
 abstract class CommandTestCase extends TestCase
 {
-    /**
-     * @var Domain\AggregateRootId
-     */
-    private $id;
-
-    /**
-     * @var InMemoryEventStore
-     */
     private $store;
-
-    /**
-     * @var UnitOfWork
-     */
     private $uow;
+    private $repository;
 
     public function setUp()
     {
         $this->store = new InMemoryEventStore();
         $this->uow = new UnitOfWork($this->store);
+        $this->repository = new EventSourcedRepository($this->createFactory(), $this->store, $this->uow);
     }
 
     public function getRepository() : Domain\Repository
     {
-        return new EventSourcedRepository($this->createFactory(), $this->store, $this->uow);
+        return $this->repository;
     }
 
-    public function with(Domain\AggregateRootId $id)
+    public function forAggregateId(Domain\AggregateRootId $id) : Given
     {
-        $this->id = $id;
-    }
-
-    public function given(Domain\Event ...$events)
-    {
-        if (\count($events) === 0) {
-            return;
-        }
-
-        $aggregate = $this->createFactory()->create($this->id);
-
-        $this->store->addEvents($aggregate, ...$events);
-    }
-
-    public function when(Application\Command $command)
-    {
-        $handler = $this->createHandler($this->store);
-        $handler->handle($command);
-    }
-
-    public function then(Domain\Event ...$expected)
-    {
-        $this->store->clear();
-        $this->uow->commit();
-
-        $aggregate = $this->createFactory()->create($this->id);
-        $actual = $this->store->getEvents($aggregate);
-
-        self::assertEquals($expected, $actual);
+        return new Specification($id, $this->createFactory(), $this->createHandler($this->store), $this->store, $this->uow);
     }
 
     abstract protected function createFactory() : Domain\AggregateRootFactory;
---
+++ src/Infrastructure/Testing/Specification.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the cbs package.
+ *
+ * (C) Alan Gabriel Bem <alan.bem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Streak\Infrastructure\Testing;
+
+use PHPUnit\Framework\Assert;
+use Streak\Application;
+use Streak\Domain;
+use Streak\Infrastructure\EventStore\InMemoryEventStore;
+use Streak\Infrastructure\Repository\UnitOfWork;
+
+/**
+ * @author Alan Gabriel Bem <alan.bem@gmail.com>
+ */
+class Specification implements Given, Then, When
+{
+    private $id;
+    private $factory;
+    private $handler;
+    private $store;
+    private $uow;
+
+    public function __construct(Domain\AggregateRootId $id, Domain\AggregateRootFactory $factory, Application\CommandHandler $handler, InMemoryEventStore $store, UnitOfWork $uow)
+    {
+        $this->id = $id;
+        $this->factory = $factory;
+        $this->handler = $handler;
+        $this->store = $store;
+        $this->uow = $uow;
+    }
+
+    public function given(Domain\Event ...$events) : When
+    {
+        if (\count($events) === 0) {
+            return $this;
+        }
+
+        $aggregate = $this->factory->create($this->id);
+
+        $this->store->addEvents($aggregate, ...$events);
+
+        return $this;
+    }
+
+    public function when(Application\Command $command) : Then
+    {
+        $this->handler->handle($command);
+
+        return $this;
+    }
+
+    public function then(Domain\Event ...$expected) : void
+    {
+        $this->store->clear();
+        $this->uow->commit();
+
+        $aggregate = $this->factory->create($this->id);
+        $actual = $this->store->getEvents($aggregate);
+
+        Assert::assertEquals($expected, $actual);
+    }
+}
+
+interface Given
+{
+    public function given(Domain\Event ...$events) : When;
+}
+
+interface When
+{
+    public function when(Application\Command $command) : Then;
+}
+
+interface Then
+{
+    public function then(Domain\Event ...$events) : void;
+}
