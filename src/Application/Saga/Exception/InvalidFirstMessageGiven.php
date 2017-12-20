faf3306ae39181369e81677f37c869de9c85c68a
Alan Gabriel Bem <alan.bem@gmail.com>
2017-12-20 16:55:03 +0100

    prepared trait facilitating saga usage

---
+++ src/Application/Saga/Exception/InvalidFirstMessageGiven.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the streak package.
+ *
+ * (C) Alan Gabriel Bem <alan.bem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Streak\Application\Saga\Exception;
+
+use Streak\Domain\Message\Exception\InvalidMessageGiven;
+
+/**
+ * @author Alan Gabriel Bem <alan.bem@gmail.com>
+ */
+class InvalidFirstMessageGiven extends InvalidMessageGiven
+{
+}
