3d0a0728bb7cc4ecc42b2fc5ee9e9dc0ab5cf734
Alan Gabriel Bem <alan.bem@gmail.com>
2018-08-03 18:53:25 +0200

    added ability to retry commands that failed because of concurrency errors

---
+++ src/Infrastructure/CommandBus/RetryingCommandBus.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the streak package.
+ *
+ * (C) Alan Gabriel Bem <alan.bem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Streak\Infrastructure\CommandBus;
+
+use Streak\Application\Command;
+use Streak\Application\CommandBus;
+use Streak\Application\CommandHandler;
+use Streak\Domain\Exception;
+
+/**
+ * @author Alan Gabriel Bem <alan.bem@gmail.com>
+ */
+class RetryingCommandBus implements CommandBus
+{
+    private $bus;
+    private $numberOfAttempts = 0;
+    private $maxAttemptsAllowed = 10;
+
+    public function __construct(CommandBus $bus, int $maxAttemptsAllowed)
+    {
+        $this->bus = $bus;
+        $this->maxAttemptsAllowed = $maxAttemptsAllowed;
+    }
+
+    public function numberOfAttempts() : int
+    {
+        return $this->numberOfAttempts;
+    }
+
+    public function maxAttemptsAllowed() : int
+    {
+        return $this->maxAttemptsAllowed;
+    }
+
+    public function register(CommandHandler $handler) : void
+    {
+        $this->bus->register($handler);
+    }
+
+    public function dispatch(Command $command) : void
+    {
+        $this->numberOfAttempts = 0;
+        try {
+            dispatch:
+                $this->numberOfAttempts++;
+            $this->bus->dispatch($command);
+        } catch (Exception\ConcurrentWriteDetected $exception) {
+            if ($this->numberOfAttempts >= $this->maxAttemptsAllowed) {
+                throw $exception;
+            }
+            goto dispatch;
+        }
+    }
+}
